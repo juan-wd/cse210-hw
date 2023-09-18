43a0d9bd7444eaf8ff45f05ac5021aaaf41ee7aa
juan-wd <coroneljcc@gmail.com>
2023-09-18 20:10:05 -0300

    mofdification to activity 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,34 +8,46 @@ class Program
         Console.WriteLine("Hello Prep2 World!");
 
         Console.WriteLine("Please enter your grade: ");
-        string grade = Console.ReadLine();
+        //string grade = Console.ReadLine();
         string letter = Console.ReadLine();
 
-        int Grade = int.Parse(grade);
+        //int Grade = int.Parse(grade);
+        int Letter = int.Parse(letter);
 
-        if (Grade >= 90)
+        string grade = "";
+
+        // if (grade >= 90)
+        if (Letter >= 90)
         {
-            Console.WriteLine("Your grade is A");
-            
+            //Console.WriteLine("Your grade is A");
+            grade = "A";
         }
-        else if (Grade >= 80)
+        //else if (Grade >= 80)
+        else if (Letter >= 80)
         {
-            Console.WriteLine("Your grade is B");
+            //Console.WriteLine("Your grade is B");
+            grade = "B";
         }
-        else if (Grade >= 70)
+        //else if (Grade >= 70)
+        else if (Letter >= 70)
         {
-            Console.WriteLine("Your grade is C");
+            //Console.WriteLine("Your grade is C");
+            grade = "C";
         }
-        else if (Grade >= 60)
+        //else if (Grade >= 60)
+        else if (Letter >= 60)
         {
-            Console.WriteLine("Your grade is D");
+            //Console.WriteLine("Your grade is D");
+            grade = "D";
         }
         else
         {
-            Console.WriteLine("Your grade is F");
+            grade = "F";
         }
 
-        if (Grade >= 70)
+            Console.WriteLine($"Your grade is {Letter}.");
+
+        if (Letter >= 70)
         {
             Console.WriteLine("Congratulations you passed this class!");
         }
