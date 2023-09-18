baad527c255f8ce81d7f8443cda5febf83a2da69
juan-wd <coroneljcc@gmail.com>
2023-09-18 17:54:01 -0300

    Prep 1 and Prep2 activities completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("Please enter your grade: ");
+        string grade = Console.ReadLine();
+        string letter = Console.ReadLine();
+
+        int Grade = int.Parse(grade);
+
+        if (Grade >= 90)
+        {
+            //Console.WriteLine("Your grade is A");
+            
+        }
+        else if (Grade >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (Grade >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (Grade >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed this class!");
+        }
+        else
+        {
+            Console.Write("You could not pass the Class, you will do it next time!");
+        }
+
     }
 }
