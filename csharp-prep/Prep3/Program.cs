aed5d2da1ab7f5249f327a6bca6ae2ca1f62a11c
juan-wd <coroneljcc@gmail.com>
2023-09-19 22:36:14 -0300

    c# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is your magic number? ");
+        string Number = Console.ReadLine();
+        int magicNumber = int.Parse(Number);
+
+        Console.WriteLine("Make your guess! ");
+        string Guess = Console.ReadLine();
+        int guessNumber = int.Parse(Guess);
+
+
+
     }
 }
