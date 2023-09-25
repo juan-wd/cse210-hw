8f30572fbb8b2d078ebd177e6baa4158b83db41e
juan-wd <coroneljcc@gmail.com>
2023-09-25 15:38:23 -0300

    excercise 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,13 +1,14 @@
 using System;
-
+using System.Linq;
 using System.Collections.Generic;
 //Console.WriteLine("Hello Prep4 World!");
 
 List<int> numbers = new List<int>();
 
+// First part, create a while loop condition to add items to the list.
 int userNumber = -1;
 
-while (userNumber != 0)
+while (userNumber != 0) 
 {
     Console.WriteLine("Indicate your list of numbers, type 0 when you have finished");
     string numbersList = Console.ReadLine();
@@ -27,21 +28,22 @@ while (userNumber != 0)
 
 }
 
+// Part 2, use foreach to perform sum caluclation
+
 int sumNumbers = 0;
 foreach (int number in numbers)
 {
     sumNumbers += number;
 }
 
-Console.WriteLine($" The sum of your numbers is: {sumNumbers}.");
+Console.WriteLine($"The sum of your numbers is: {sumNumbers}.");
 
+// Part 3, count items in the list to perform average calculation of the total of items
 
-int averageNumbers = 0;
-float numberDecimal = (float)sumNumbers;
+float averageDecimal = (float)sumNumbers / numbers.Count;
+Console.WriteLine($"The average of numbers is {averageDecimal}");
 
-for (int i = 0; i < numbers.Count; i++)
-{
-    averageNumbers = (int)(numberDecimal / i);
-}
-Console.WriteLine($"The average of numbers is {averageNumbers}");
+// Part 4, I found on https://www.techiedelight.com/ the usage of "System.Linq" to perform Max value method.
+
+Console.WriteLine("The maximum number is: "+ numbers.Max());
 
