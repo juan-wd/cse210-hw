b4489b2e4dcdb20bb754059f7ac40cf26cc22da0
juan-wd <coroneljcc@gmail.com>
2023-09-25 16:35:38 -0300

    excersise 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");
+
+        void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program");
+        
+        }
+
+        void DisplayUserName(string Name)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {userName}");
+        }
     }
 }
