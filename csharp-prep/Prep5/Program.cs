67df3ea8a118898ed8fc921f2a2c826695fbc5ef
juan-wd <coroneljcc@gmail.com>
2023-09-25 23:12:52 -0300

    excersise 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,23 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
 
-        void DisplayMessage()
-        {
-            Console.WriteLine("Welcome to the Program");
+        string userName = UserName();
+
+        int userNumber = UserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squareNumber);
+    }
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program");          
+    }
+    static string UserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int UserNumber()
+    {
+        Console.WriteLine("Please indicate your favorite number");
+        string number = Console.ReadLine();
+        int Number = int.Parse(number);
         
-        }
+        return Number;
+    }
 
-        void DisplayUserName(string Name)
-        {
-            Console.WriteLine("Please enter your name: ");
-            string userName = Console.ReadLine();
+    static int SquareNumber(int Number)
+    {
+        int Square = Number * Number;
+        return Square;
+    }
 
-            Console.WriteLine($"Your name is {userName}");
-        }
+    static void DisplayResult(string name, int Square)
+    {
+    Console.WriteLine($"{name}, the square root of your number is {Square}.");
     }
 }
