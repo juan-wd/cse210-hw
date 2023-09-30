eeda2eadedc7c19d15a8899f3d5c1bbf1bce29e8
juan-wd <coroneljcc@gmail.com>
2023-09-29 21:21:41 -0300

    test

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Apex Inteliserve";
+        job1._jobTitle = "Sales Executive";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+
+        job1._company = "Mangi Sales Co";
+        job1._jobTitle = "Purchasing mamanger";
+        job1._startYear = 2018;
+        job1._endYear = 2020;
+
+
     }
 }
